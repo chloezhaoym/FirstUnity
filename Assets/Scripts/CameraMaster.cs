fa092fb1b6866bac73467d504e023f648cc04b2d
Yiming Zhao <chloezhaoym@gmail.com>
2020-07-20 23:33:10 -0700

    Move camera position

--- Assets/Scripts/CameraMaster.cs
+++ Assets/Scripts/CameraMaster.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraMaster : MonoBehaviour
 {
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class CameraMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            //cam.transform.position=cam.transform.position+ new Vector3(1,0,0);
+
+            var position=cam.transform.position;
+            position.x+=1;
+            cam.transform.position=position;
+
+            
+        }
+
     }
 }
